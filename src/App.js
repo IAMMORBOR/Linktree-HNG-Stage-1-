2bda91efb0c1f5f8b622bdc9bf6d9594f9cd8d93
IAMMORBOR <ofamotibe@gmail.com>
2022-10-27 19:52:26 +0100

    fixed the mobile section

--- src/App.js
+++ src/App.js
@@ -5,10 +5,23 @@ import zuriLogo from './image-container/zuriLogo.png';
 import slack from './image-container/slack.png';
 import SocialLink from './Button-component/link.component';
 import {BsGithub} from 'react-icons/bs';
+import {HiOutlineDotsCircleHorizontal} from 'react-icons/hi';
+import {MdOutlineDarkMode} from 'react-icons/md';
+
+
 
 function App() {
   return (
+
    <section className='MainSection'>
+    <nav className='MainSection-Nav'>
+      <div className='MainSection-Nav--desktop'>
+        <MdOutlineDarkMode className='MainSection-Nav--icon'/>
+      </div>
+      <div className='MainSection-Nav--mobile'>
+      <HiOutlineDotsCircleHorizontal className='MainSection-Nav--icon' />
+      </div>
+    </nav>
     <section className='linktree-section'>
       <div id='profile'>
         <img id='profile__img' src={ProfilePicture}alt='profile__img'/>
@@ -24,22 +37,17 @@ function App() {
       </div>
     </section>
     <section className='SocialMedia-section'>
+      <a href='https://github.com/IAMMORBOR'>
         <img className='SocialMedia-section--slack' src={slack} alt='slack--icon'/>
+      </a>
         <BsGithub className='SocialMedia-section--github' />
     </section>
     <div className='empty'></div>
-
     <footer className='footer-section'>
-    <img className='footer-section--logo' src={zuriLogo} alt='slack--icon'/>
-    <p className='footer-section-text'>HNG Internship 9 Frontend Task</p>
-    <img className='footer-section--Ingressivelogo' src={logo1} alt='slack--icon'/>
-
-      
+      <img className='footer-section--logo' src={zuriLogo} alt='slack--icon'/>
+      <p className='footer-section-text'>HNG Internship 9 Frontend Task</p>
+      <img className='footer-section--Ingressivelogo' src={logo1} alt='slack--icon'/>
     </footer>
-   
-  
-
-
    </section>
   );
 }
