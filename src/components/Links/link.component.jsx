73444851ce569dbe65f945a3c598f3d6d636e643
IAMMORBOR <ofamotibe@gmail.com>
2022-10-30 13:28:59 +0100

    removed the twitter icon

---
+++ src/components/Links/link.component.jsx
@@ -0,0 +1,9 @@
+// import './link.style.scss';
+
+// const SocialLink=(props)=>{
+//     return(
+//         <a className={`Data-links ${'btn__'+props.btnid}`} href={props.link}> {props.children}</a>
+//     )
+// }
+// export default SocialLink;
+
--- src/components/Profile.component.jsx
+++ src/components/Profile.component.jsx
@@ -1,16 +1,13 @@
 import ProfilePicture from '../image-container/Profileimg.png';
-import {BsTwitter}from 'react-icons/bs'
 
 const UserProfile=()=>{
     return(
         <div id='profile'>
             <img id='profile__img' src={ProfilePicture}alt='profile__img'/>
-            <p className="slack" id='slack'>Morbor</p>
-            <div id='profile--twitter-section'>
-                <BsTwitter id='profile--twitter-section--icon'/>
-                <h4 id='twitter'>@datgirlMORBOR</h4>
-            </div>
-      </div>
+            <p id='slack'>Morbor</p>
+            <h4 id='twitter'>datgirlMORBOR</h4>
+        </div>
+
     )
 }
 export default UserProfile;
